5ce5dcd2a741b5d542d49ee0f892d16e388eeb58
Eliel de Souza Faria <elielrj@gmail.com>
2023-03-26 09:00:58 -0300

    Solução encontrada para transformar a classe em ENUM

---
+++ application/libraries/Motivo.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Motivo{
+
+        static $PARTICULAR = 'Particular';
+        static $SERVICO = 'Serviço';
+    
+}
+
+
+?>
---
+++ application/libraries/NivelDeAcesso.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class NivelDeAcesso{
+
+        static $LER = "Ler";
+        static $ESCREVER = "Escrever";
+        static $DESPACHAR = "Despachar";
+        static $ADMINISTRAR = "Administrar";
+        static $ROOT = "Root";
+
+    }
+?>
