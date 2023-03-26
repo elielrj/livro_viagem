ff90368da78366631b3ecbbc688284e244f9be36
Eliel de Souza Faria <elielrj@gmail.com>
2023-03-26 09:04:06 -0300

    Update de diagramas

--- diagramas/SQL Livro viagem.sql
+++ diagramas/SQL Livro viagem.sql
@@ -2,216 +2,117 @@
 drop database livro_viagem;
 drop database mydb;
 
--- MySQL Workbench Forward Engineering
+-- MySQL Workbench Synchronization
+-- Generated: 2023-03-25 15:06
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: esfso
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema livro_viagem
--- -----------------------------------------------------
+ALTER TABLE `livro_viagem`.`Cidade` 
+DROP FOREIGN KEY `fk_Cidade_Estado`;
 
--- -----------------------------------------------------
--- Schema livro_viagem
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `livro_viagem` DEFAULT CHARACTER SET utf8 ;
-USE `livro_viagem` ;
+ALTER TABLE `livro_viagem`.`Endereco` 
+DROP FOREIGN KEY `fk_Endereco_Cidade1`;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Estado`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Estado` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `sigla` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `nome_UNIQUE` (`nome` ASC) VISIBLE,
-  UNIQUE INDEX `sigla_UNIQUE` (`sigla` ASC) VISIBLE)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Usuario` 
+DROP FOREIGN KEY `fk_Usuario_Funcao1`;
 
+ALTER TABLE `livro_viagem`.`Telefone` 
+DROP FOREIGN KEY `fk_Telefone_Usuario1`;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Cidade`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Cidade` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `estadoId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Cidade_Estado_idx` (`estadoId` ASC) VISIBLE,
-  CONSTRAINT `fk_Cidade_Estado`
-    FOREIGN KEY (`estadoId`)
-    REFERENCES `livro_viagem`.`Estado` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Viagem` 
+DROP FOREIGN KEY `fk_Viagem_Endereco1`;
 
+ALTER TABLE `livro_viagem`.`Funcao` 
+DROP FOREIGN KEY `fk_Funcao_NivelDeAcesso1`;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Bairro`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Bairro` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `cidadeId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Bairro_Cidade1_idx` (`cidadeId` ASC) VISIBLE,
-  CONSTRAINT `fk_Bairro_Cidade1`
-    FOREIGN KEY (`cidadeId`)
-    REFERENCES `livro_viagem`.`Cidade` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Usuario` 
+CHANGE COLUMN `email` `email` VARCHAR(100) NOT NULL AFTER `nome`,
+CHANGE COLUMN `senha` `senha` VARCHAR(100) NOT NULL AFTER `email`,
+CHANGE COLUMN `status` `status` TINYINT(4) NOT NULL AFTER `ultimoAcesso`;
 
+ALTER TABLE `livro_viagem`.`Telefone` 
+CHANGE COLUMN `contato` `contato` ENUM("Emergência", "Localização") NOT NULL ;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Hierarquia`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Hierarquia` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `postoOuGraduacao` VARCHAR(100) NOT NULL,
-  `sigla` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `postoOuGraduacao_UNIQUE` (`postoOuGraduacao` ASC) VISIBLE,
-  UNIQUE INDEX `sigla_UNIQUE` (`sigla` ASC) VISIBLE)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Viagem` 
+CHANGE COLUMN `usuarioId` `usuarioId` INT(11) NOT NULL AFTER `analisada`,
+CHANGE COLUMN `enderecoId` `enderecoId` INT(11) NOT NULL AFTER `usuarioId`,
+CHANGE COLUMN `territorio` `territorio` ENUM("Nacional", "Internacional") NOT NULL ,
+CHANGE COLUMN `motivo` `motivo` ENUM("Particular", "Serviço") NOT NULL ;
 
+ALTER TABLE `livro_viagem`.`Funcao` 
+DROP COLUMN `nivelDeAcessoId`,
+ADD COLUMN `nivelDeAcesso` ENUM("Ler", "Escrever", "Despachar", "Administrar", "Root") NOT NULL AFTER `nome`,
+CHANGE COLUMN `descricao` `nome` VARCHAR(45) NOT NULL ,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`id`),
+DROP INDEX `fk_Funcao_NivelDeAcesso1_idx` ;
+;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`NivelDeAcesso`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`NivelDeAcesso` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `poder` ENUM("Ler", "Escrever", "Despachar", "Administrar") NOT NULL,
-  `status` TINYINT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `livro_viagem`.`niveldeacesso` ;
 
+ALTER TABLE `livro_viagem`.`Cidade` 
+ADD CONSTRAINT `fk_Cidade_Estado`
+  FOREIGN KEY (`estadoId`)
+  REFERENCES `livro_viagem`.`Estado` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Funcao`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Funcao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NOT NULL,
-  `status` TINYINT NOT NULL,
-  `nivelDeAcessoId` INT NOT NULL,
-  PRIMARY KEY (`id`, `nivelDeAcessoId`),
-  INDEX `fk_Funcao_NivelDeAcesso1_idx` (`nivelDeAcessoId` ASC) VISIBLE,
-  CONSTRAINT `fk_Funcao_NivelDeAcesso1`
-    FOREIGN KEY (`nivelDeAcessoId`)
-    REFERENCES `livro_viagem`.`NivelDeAcesso` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Endereco` 
+DROP FOREIGN KEY `fk_Endereco_Usuario1`;
 
+ALTER TABLE `livro_viagem`.`Endereco` ADD CONSTRAINT `fk_Endereco_Usuario1`
+  FOREIGN KEY (`usuarioId`)
+  REFERENCES `livro_viagem`.`Usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Endereco_Cidade1`
+  FOREIGN KEY (`cidadeId`)
+  REFERENCES `livro_viagem`.`Cidade` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Usuario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `status` TINYINT NOT NULL,
-  `dataDeCriacao` DATETIME NOT NULL,
-  `ultimoAcesso` DATETIME NOT NULL,
-  `hierarquiaId` INT NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `senha` VARCHAR(100) NOT NULL,
-  `funcaoId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Usuario_Hierarquia1_idx` (`hierarquiaId` ASC) VISIBLE,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  INDEX `fk_Usuario_Funcao1_idx` (`funcaoId` ASC) VISIBLE,
-  CONSTRAINT `fk_Usuario_Hierarquia1`
-    FOREIGN KEY (`hierarquiaId`)
-    REFERENCES `livro_viagem`.`Hierarquia` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Usuario_Funcao1`
-    FOREIGN KEY (`funcaoId`)
-    REFERENCES `livro_viagem`.`Funcao` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Usuario` 
+DROP FOREIGN KEY `fk_Usuario_Hierarquia1`;
 
+ALTER TABLE `livro_viagem`.`Usuario` ADD CONSTRAINT `fk_Usuario_Hierarquia1`
+  FOREIGN KEY (`hierarquiaId`)
+  REFERENCES `livro_viagem`.`Hierarquia` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Usuario_Funcao1`
+  FOREIGN KEY (`funcaoId`)
+  REFERENCES `livro_viagem`.`Funcao` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Endereco` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `logradouro` VARCHAR(100) NOT NULL,
-  `numero` VARCHAR(100) NOT NULL,
-  `bairroId` INT NOT NULL,
-  `usuarioId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Endereco_Bairro1_idx` (`bairroId` ASC) VISIBLE,
-  INDEX `fk_Endereco_Usuario1_idx` (`usuarioId` ASC) VISIBLE,
-  CONSTRAINT `fk_Endereco_Bairro1`
-    FOREIGN KEY (`bairroId`)
-    REFERENCES `livro_viagem`.`Bairro` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Endereco_Usuario1`
-    FOREIGN KEY (`usuarioId`)
-    REFERENCES `livro_viagem`.`Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Telefone` 
+ADD CONSTRAINT `fk_Telefone_Usuario1`
+  FOREIGN KEY (`usuarioId`)
+  REFERENCES `livro_viagem`.`Usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
+ALTER TABLE `livro_viagem`.`Viagem` 
+DROP FOREIGN KEY `fk_Viagem_Usuario1`;
 
--- -----------------------------------------------------
--- Table `livro_viagem`.`Telefone`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Telefone` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `numero` VARCHAR(100) NOT NULL,
-  `contato` ENUM("EMERGENCIA", "LOCALIZACAO") NOT NULL,
-  `parentescoDoContato` VARCHAR(100) NOT NULL,
-  `usuarioId` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Telefone_Usuario1_idx` (`usuarioId` ASC) VISIBLE,
-  CONSTRAINT `fk_Telefone_Usuario1`
-    FOREIGN KEY (`usuarioId`)
-    REFERENCES `livro_viagem`.`Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `livro_viagem`.`Viagem`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livro_viagem`.`Viagem` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `aprovada` TINYINT NOT NULL,
-  `territorio` ENUM("NACIONAL", "INTERNACIONAL") NOT NULL,
-  `motivo` ENUM("PARTICULAR", "SERVICO") NOT NULL,
-  `usuarioId` INT NOT NULL,
-  `enderecoId` INT NOT NULL,
-  `dataIda` DATE NOT NULL,
-  `dataVolta` DATE NOT NULL,
-  `observacao` VARCHAR(250) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Viagem_Usuario1_idx` (`usuarioId` ASC) VISIBLE,
-  INDEX `fk_Viagem_Endereco1_idx` (`enderecoId` ASC) VISIBLE,
-  CONSTRAINT `fk_Viagem_Usuario1`
-    FOREIGN KEY (`usuarioId`)
-    REFERENCES `livro_viagem`.`Usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Viagem_Endereco1`
-    FOREIGN KEY (`enderecoId`)
-    REFERENCES `livro_viagem`.`Endereco` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `livro_viagem`.`Viagem` ADD CONSTRAINT `fk_Viagem_Usuario1`
+  FOREIGN KEY (`usuarioId`)
+  REFERENCES `livro_viagem`.`Usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Viagem_Endereco1`
+  FOREIGN KEY (`enderecoId`)
+  REFERENCES `livro_viagem`.`Endereco` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
